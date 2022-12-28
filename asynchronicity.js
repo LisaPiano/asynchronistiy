6ee935e25a291636d76bf3d57bf63e481d172430
Lisa Campi Walters <campi_l@fortlewis.edu>
2022-12-27 21:55:38 -0700

    added two types of setTimeout methods

--- asynchronicity.js
+++ asynchronicity.js
@@ -1,16 +1,25 @@
-function func3() {
-  console.log('Guys!');
-}
+// function func3() {
+//   console.log('Guys!');
+// }
 
-function func2() {
-  console.log('you');
-}
+// function func2() {
+//   console.log('you');
+// }
 
-function func1() {
-  console.log('Hey');
-  func2();
-  func3();
-}
+// function func1() {
+//   console.log('Hey');
+//   func2();
+//   func3();
+// }
 
-func1();  
+// func1();  
 
+// function sayHi(){
+//     console.log("Hi THere!");
+// }
+
+// setTimeout(sayHi, 8000);
+
+setTimeout(() => {
+    console.log("SURPRISE!");
+}, 8000);
